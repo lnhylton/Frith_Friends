603d7a847c783722452527c01b3d0ff43086057d
Claire Doody <cdoody@vt.edu>
2023-10-17 21:27:34 -0400

    front end

--- frith-inventory-frontend/src/App.jsx
+++ frith-inventory-frontend/src/App.jsx
@@ -5,13 +5,38 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  // POST request using fetch()
+  fetch("http://localhost:5000/add", {
+
+    // Adding method type
+    method: "POST",
+
+    // Adding body or contents to send
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1
+    
+    }),
+
+    // Adding headers to the request
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+
+    // Converting to JSON
+    .then(response => response.json())
+
+    // Displaying results to console
+    .then(json => console.log(json));
 
   return (
     <div className="App">
       <div className='App-header'>
         <h1>Frith Friends</h1>
-    </div>
-      
+      </div>
+
     </div>
   )
 }
