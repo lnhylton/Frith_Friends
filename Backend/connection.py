620510574f5486111e79eaa050ab372657b391f2
ColtonTshudy <82547877+ColtonTshudy@users.noreply.github.com>
2023-10-17 21:21:02 -0400

    added cors

--- Backend/connection.py
+++ Backend/connection.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_cors import CORS
 import mysql.connector
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 config = {
     'host': 'localhost',
@@ -10,9 +14,9 @@ config = {
     'database': 'frith_friends'
 }
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    pass
 
 @app.route('/add', methods=['POST'])
 def add():
