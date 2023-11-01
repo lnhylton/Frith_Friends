a9b76ff81035bc3f8b0a41f33100d9eafacaf6bf
Luke Hylton <lnhylton@vt.edu>
2023-10-31 23:19:49 -0400

    Added update function and created frontend for it

--- Backend/connection.py
+++ Backend/connection.py
@@ -7,7 +7,7 @@ CORS(app)
 
 config = {
     'host': 'localhost',
-    'user': 'Admin',
+    'user': 'root', # Change this to admin if necessary
     'password': 'frith',
     'database': 'frith_friends'
 }
@@ -28,6 +28,41 @@ def add():
     conn.close()
     response = {"status": "success"}
     return jsonify(response)
+
+@app.route('/update', methods=['PUT'])
+def update():
+    #try:
+    print(request.json)
+    data = request.json
+    table_id_col = get_item_ids(data['tableName'])
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+
+    # Using parameterized query to update a specific item by its ID
+    cursor.execute(f'UPDATE {data['tableName']} SET {data['updateData']} WHERE {table_id_col} = {data['itemId']}')
+    conn.commit()
+    cursor.close()
+    conn.close()
+    response = {"status": "success"}
+    #except Exception as e:
+    #    response = {"status": "error", "message": str(e)}
+    return jsonify(response)
+
+def get_item_ids(table_name):
+    # Define a dictionary to map table names to their respective ID column names
+    table_to_id_column = {
+        "consumable": "c_id",
+        "consumable_location": "c_id",
+        "machine": "m_id",
+        "machine_location": "m_id",
+        "non_consumable": "nc_id",
+        "non_consumable_location": "nc_id",
+        "room": "r_id",
+        "storage_medium": "sm_id",
+    }
+
+    return table_to_id_column[table_name]
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frith-inventory-frontend/src/App.jsx
+++ frith-inventory-frontend/src/App.jsx
@@ -1,81 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+  const [data, setData] = useState("");
+  const [latestResponse, setLatestResponse] = useState("");
+  const [tableName, setTableName] = useState("consumable"); // Default value
+  const [itemId, setItemId] = useState("");
+  const [updateData, setUpdateData] = useState("");
 
-  const [count, setCount] = useState(0)
-  const [data, setData] = useState("")
-  const [latestResponse, setLatestResponse] = useState("")
-
-  const testInput = event => {
+  const handleDataChange = (event) => {
     setData(event.target.value);
   };
 
-  const testSubmit = event => {
-    // POST request using fetch()
-    fetch("http://localhost:5000/add", {
+  const handleTableNameChange = (event) => {
+    setTableName(event.target.value);
+  };
 
-      // Adding method type
-      method: "POST",
+  const handleItemIdChange = (event) => {
+    setItemId(event.target.value);
+  };
 
-      // Adding body or contents to send
-      body: JSON.stringify({
-        data: data
+  const handleUpdateDataChange = (event) => {
+    setUpdateData(event.target.value);
+  };
 
-      }),
-
-      // Adding headers to the request
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    })
-
-      // Converting to JSON
-      .then(response => response.json())
-
-      // Displaying results to console
-      .then(res => {
-        setLatestResponse(res.status)
-        console.log(res)
+  const handleDataSubmit = () => {
+    if (data) {
+      // Perform your API call here for adding data
+      fetch("http://localhost:5000/add", {
+        method: "POST",
+        body: JSON.stringify({
+          data: data
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
       })
-      .catch((e) => {
-        setLatestResponse("failed")
+        .then((response) => response.json())
+        .then((res) => {
+          setLatestResponse(res.status);
+          console.log(res);
+        })
+        .catch((e) => {
+          setLatestResponse("failed");
+        });
+    }
+  };
+
+  const handleUpdateSubmit = () => {
+    if (tableName && itemId && updateData) {
+      // Perform your API call here for updating data
+      fetch("http://localhost:5000/update", {
+        method: "PUT",
+        body: JSON.stringify({
+          tableName: tableName,
+          itemId: itemId,
+          updateData: updateData
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
       })
-  }
-
-
+        .then((response) => response.json())
+        .then((res) => {
+          setLatestResponse(res.status);
+          console.log(res);
+        })
+        .catch((e) => {
+          setLatestResponse("failed");
+        });
+    }
+  };
 
   return (
     <div className="App">
-      <div className='App-header'>
+      <div className="App-header">
         <h1>Frith Friends</h1>
-
       </div>
-      <label>This will add an item to "consumables" table</label>
-
-      <input className="input"
+      <label>This will add an item to the consumable table:</label>
+      <input
+        className="input"
         type="database"
-        name="database"
-        onChange={testInput}
+        placeholder="Data to Add"
+        onChange={handleDataChange}
         value={data}
       />
-      <button type="submit" onClick={testSubmit} >Submit</button>
-      {
-        latestResponse === "success" ?
-          <label className="success">Success!</label>
-          :
-          <></>
-      }
-      {
-        latestResponse === "failed" ?
-          <label className="success">Failed!</label>
-          :
-          <></>
-      }
+      <button type="submit" onClick={handleDataSubmit}>
+        Submit Add
+      </button>
+
+      <label>This will update an item to the selected table:</label>
+      <select
+        className="input"
+        onChange={handleTableNameChange}
+        value={tableName}
+      >
+        <option value="consumable">consumable</option>
+        <option value="consumable_location">consumable_location</option>
+        <option value="machine">machine</option>
+        <option value="machine_location">machine_location</option>
+        <option value="non_consumable">non_consumable</option>
+        <option value="non_consumable_location">non_consumable_location</option>
+        <option value="room">room</option>
+        <option value="storage_medium">storage_medium</option>
+      </select>
+      <input
+        className="input"
+        type="text"
+        placeholder="Item ID"
+        onChange={handleItemIdChange}
+        value={itemId}
+      />
+      <input
+        className="input"
+        type="text"
+        placeholder="Update Data"
+        onChange={handleUpdateDataChange}
+        value={updateData}
+      />
+      <button type="submit" onClick={handleUpdateSubmit}>
+        Submit Update
+      </button>
+
+      {latestResponse === "success" ? (
+        <label className="success">Success!</label>
+      ) : latestResponse === "failed" ? (
+        <label className="error">Failed!</label>
+      ) : null}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
