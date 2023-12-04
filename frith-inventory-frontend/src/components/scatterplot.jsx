2a5ab6c998c225af1e0a7b76fc8d924d70ea664e
Claire Doody <cdoody@vt.edu>
2023-12-04 10:24:30 -0500

    edits to put johans stuff in:

--- frith-inventory-frontend/src/components/scatterplot.jsx
+++ frith-inventory-frontend/src/components/scatterplot.jsx
@@ -6,7 +6,7 @@ const ScatterPlot = ({ data }) => {
 
   useEffect(() => {
     // Set up the SVG container
-    const margin = { top: 20, right: 20, bottom: 40, left: 40 };
+    const margin = { top: 20, right: 20, bottom: 75, left: 45 };
     const width = 500 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
 
@@ -36,12 +36,23 @@ const ScatterPlot = ({ data }) => {
       .style('fill', 'steelblue');
 
     // Create axes
-    const xAxis = d3.axisBottom(xScale);
+    const xAxis = d3.axisBottom(xScale)
+    .tickFormat(d3.timeFormat('%Y-%m-%d %H:%M:%S'))
+    .tickPadding(10) // Adjust the padding between ticks and labels
+    .tickSize(0) // Set tick size to 0 to hide ticks initially
+    .tickSizeInner(0) // Set inner tick size to 0
+    .tickSizeOuter(0); // Set outer tick size to 0
     const yAxis = d3.axisLeft(yScale);
 
+  
     svg.append('g')
       .attr('transform', `translate(0,${height})`)
-      .call(xAxis);
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", ".15em")
+      .attr("transform", "rotate(-65)");
 
     svg.append('g')
       .call(yAxis);
